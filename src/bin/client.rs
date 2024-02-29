9a1b836be978d58b582fb83ec0f9d1d1b02a4813
DroneBreaker <brownierz01@gmail.com>
2024-02-29 12:19:00 +0000

    Initialized the app

---
+++ src/bin/client.rs
@@ -0,0 +1,10 @@
+use async_std::io::BufReader;
+use async_std::prelude::*;
+use async_std::{task, io, net};
+use std::sync::Arc;
+use chat::utils::{self, ChatResult};
+use chat::{Client, Server};
+
+fn main() {
+    
+}
